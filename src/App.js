d800612f1df5112c86747a5db56949236530820e
Rodrigo Xavier <rodrfx@gmail.com>
2020-10-21 23:22:34 -0300

    criaçao de layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Cabecalho } from './components/cabecalho'
+import { Produtos } from './components/produtos'
+import { Rodape } from "./components/rodape"
+import { Filtro } from "./components/filtro"
+import logo from "./img/logo.png"
+import lupa from "./img/lupa.png"
+import sacola from "./img/sacola.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Cabecalho
+            imagem={logo}
+            lupa={lupa}
+            sacola={sacola}
+          />
+        </header>
+
+        <hr />
+
+        <section>
+          <Filtro />
+        </section>
+
+        <hr />
+
+        <section className="produtos">
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 1"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 2"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 3"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 4"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 5"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 6"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 7"}
+          />
+          <Produtos
+            imagem={"https://picsum.photos/100/101"}
+            titulo={"Camiseta 8"}
+          />
+        </section>
+
+        <hr />
+
+        <footer>
+          <Rodape />
+        </footer>
+
+      </div>
+    )
+  }
 }
 
 export default App;
